0f404f9a3d9f6adc58ad5284d0f45c0662e7c933
Jamerson Lima / <101479828+jamisu337@users.noreply.github.com>
2023-07-27 21:04:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tela-de-login---botao-de-redes-sociais
+ uma simples tela de login, coloquei um botao onde lava para as minhas redes sociais
---
+++ script.js
@@ -0,0 +1,5 @@
+function onclickmenu(){
+    document.getElementById("menu").classList.toggle("change")
+    document.getElementById("nav").classList.toggle("change")
+    document.getElementById("menu-bg").classList.toggle("change-bg")
+}
